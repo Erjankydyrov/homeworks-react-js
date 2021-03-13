03e0504aa00d61c3f3560dab22d1ac4d2226cc53
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-13 23:10:26 +0600

    added styles

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,11 +1,12 @@
 import classes from "./Person.module.css";
 
-const Person = ({name, role, wpm, commits}) => {
+const Person = ({name, photo, role, wpm, commits}) => {
         return (
             <div className={classes.Person}>
+                <div className={classes.divPhoto}><img src={photo} alt="Photo" className={classes.photo}></img></div>
                 <div className={classes.name}>{name}</div>
-                <div className={classes.role}>{role}</div>
-                <div className={classes.wpm}>{wpm}WPM</div>
+                <div className={classes.role}>Role: {role}</div>
+                <div className={classes.wpm}>{wpm} WPM</div>
                 <div className={classes.commits}>{commits} commits</div>
             </div>
         )
