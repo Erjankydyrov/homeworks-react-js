07ab30a1802e02af6d2557b2aa0372c6a27c6423
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-13 21:44:46 +0600

    corected App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import BiggerNumber from "./components/BiggerNumber";
-import LiseItems from "./components/LizeItems";
-import RandomNumbers from "./components/RandomNumbers";
-import Sum from "./components/Summa";
+
 
 function App() {
   return (
     <div className="App">
-     <Sum a={5} b={7} />
-     <RandomNumbers min={5} max={10} />
-     <BiggerNumber a={5} b={7} />
-     <LiseItems list = {['Apple', 'Pear', 'Limon', 'Tomato', "Ananas", "Banana"]}></LiseItems>
+     
     </div>
   );
 }
