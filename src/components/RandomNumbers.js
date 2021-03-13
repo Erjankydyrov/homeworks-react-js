880e06edca7581c0eae95d9f4a5d9f93f6d8f45a
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-13 21:44:22 +0600

    delete old homework

--- src/components/RandomNumbers.js
+++
@@ -1,6 +0,0 @@
-function RandomNumbers(props) {
-    return <div className="Rand">Random number = { props.min + (Math.round(Math.random() * props.max)) }</div>
-}
-
-export default RandomNumbers;
-   
--- src/components/Summa.js
+++
@@ -1,5 +0,0 @@
-function Sum(props) {
-    return <div className="Sum">Summa = { props.a + props.b }</div>
-}
-  
-export default Sum;
