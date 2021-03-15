04270b200148b9cb0f5f37ddbaec465842adaa5a
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-15 13:51:11 +0600

    corected img

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,7 +3,7 @@ import classes from "./Person.module.css";
 const Person = ({name, photo, role, wpm, commits}) => {
         return (
             <div className={classes.Person}>
-                <div className={classes.divPhoto}><img src={photo} alt="Photo" className={classes.photo}></img></div>
+                <div className={classes.divPhoto}><img src={photo} alt="Photo" className={classes.photo} /></div>
                 <div className={classes.name}>{name}</div>
                 <div className={classes.role}>Role: {role}</div>
                 <div className={classes.wpm}>{wpm} WPM</div>
